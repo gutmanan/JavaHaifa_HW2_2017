aff656dbe31bfee3f39d9b6387e60d4df60afbb5
Shai Gutman <Shai Gutman@ShaiGutman-PC>
2017-04-13 12:38:15 +0300

    A project was uploaded

--- JavaHw_2_students/src/Animal.java
+++ JavaHw_2_students/src/Animal.java
@@ -1,41 +1,41 @@
 /**
  * Animal type: Parrot, Dog, Cat etc...
+ * @author Java spring 2017
  */
 
-/**
- * @author Java spring 2017
-	public class Animal {
-	//TODO Determine the access level of the class members.
-	// Replace all **** with public, private or protected
-	/**
-	 * The animal's species
-	 */
-	public String species;
-	/**
-	 * The animal's category
-	 * @see E_Category
-	 */
-	**** E_Category category;
-	/**
-	 * Constructor
-	 * @param species Animal species
-	 * @param category Animal Category
-	 */
-	**** Animal(String species, E_Category category) {
-		//TODO Complete this method
-	}
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "Animal [species=" + species + ", category=" + category + "]";
-	}
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		//TODO Complete this method
-	}
+public class Animal {
+    // TODO Determine the access level of the class members.
+    // Replace all **** with public, private or protected
+    /**
+     * The animal's species
+     */
+    private String species;
+    /**
+     * The animal's category
+     * @see E_Category
+     */
+    private E_Category category;
+    /**
+     * Constructor
+     * @param species Animal species
+     * @param category Animal Category
+     */
+    public Animal(String species, E_Category category) {
+            this.species = species;
+            this.category = category;
+    }
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+            return "Animal [species=" + species + ", category=" + category + "]";
+    }
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+            //TODO Complete this method
+    }
 }
